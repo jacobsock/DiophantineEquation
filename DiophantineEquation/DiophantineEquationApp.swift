ee69b0b5403ec2ebd0c1778f69b23bbd532557a2
Jacob M Sock <jacobmsock@gmail.com>
2023-03-24 16:32:31 -0400

    Initial Commit

---
+++ DiophantineEquation/DiophantineEquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiophantineEquationApp.swift
+//  DiophantineEquation
+//
+//  Created by Jacob Sock on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DiophantineEquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
